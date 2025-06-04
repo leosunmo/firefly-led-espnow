5e1860f858a998545746c154831089d7a2c90007
leo sunmo <leo.sunmo@gmail.com>
2025-06-04 16:42:35 +0200

    Update pins and button pins

--- main/config.h
+++ main/config.h
@@ -10,12 +10,12 @@
 #define POT_CENTER_THRESHOLD 200    // Threshold around center position (0-4095)
 
 // Encoder configuration
-#define ENCODER_COLOR_A_PIN 8       // Color encoder signal A
-#define ENCODER_COLOR_B_PIN 9       // Color encoder signal B
+#define ENCODER_COLOR_A_PIN 20       // Color encoder signal A
+#define ENCODER_COLOR_B_PIN 19       // Color encoder signal B
 #define ENCODER_COLOR_BTN_PIN 10    // Color encoder button (push)
-#define ENCODER_PATTERN_A_PIN 11    // Pattern encoder signal A
-#define ENCODER_PATTERN_B_PIN 12    // Pattern encoder signal B
-#define ENCODER_PATTERN_BTN_PIN 13  // Pattern encoder button (push)
+#define ENCODER_PATTERN_A_PIN 22    // Pattern encoder signal A
+#define ENCODER_PATTERN_B_PIN 21    // Pattern encoder signal B
+#define ENCODER_PATTERN_BTN_PIN 11  // Pattern encoder button (push)
 #define ENCODER_POLL_INTERVAL_MS 10 // Read encoder every 10ms
 #define ENCODER_DEBOUNCE_TIME_MS 20 // Button debounce time
 #define ENCODER_LONG_PRESS_MS 1000  // Long press time for encoder buttons
@@ -27,7 +27,7 @@ typedef enum {
 
 // Set the device role
 #ifndef DEVICE_ROLE
-#define DEVICE_ROLE DEVICE_ROLE_RECEIVER
+#define DEVICE_ROLE DEVICE_ROLE_SENDER
 #endif
 
 #define USE_POINT_TO_POINT true
@@ -36,8 +36,8 @@ typedef enum {
 #define RECEIVER_LOG_LEVEL ESP_LOG_DEBUG
 #define UART_LOG_LEVEL ESP_LOG_DEBUG
 
-#define BUTTONBLUE_GPIO_NUM 16
-#define BUTTONRED_GPIO_NUM 17
+#define BUTTONBLUE_GPIO_NUM 10
+#define BUTTONRED_GPIO_NUM 11
 
 // UART Configuration for RP2040 communication
 #define UART_TX_PIN 21 // TX pin to RP2040, default U0TXD pin
